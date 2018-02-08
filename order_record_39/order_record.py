e117335e7300460c801dc981fe777d8928f17ab6
jay <weeneen02@gmail.com>
2018-02-09 00:45:31 +0900

    update 38, 39 solutions

---
+++ order_record_39/order_record.py
@@ -0,0 +1,50 @@
+# -*- encoding:utf-8 -*-
+
+'''
+다음의 데이터 집합이 주어졌다고 가정하자.
+
+Firstname   |   LastName    |   Position            |   SeparationDate
+-----------------------------------------------------------------------
+john        |   johnson     |   Manager             |   2016-12-31
+-----------------------------------------------------------------------
+Tou         |   Xiong       |   Software Engineer   |   2016-10-15
+-----------------------------------------------------------------------
+Michaela    |   Michaelson  |   District Manager    |   2015-12-19
+-----------------------------------------------------------------------
+Jake        |   Jacobson    |   Programmer          |
+-----------------------------------------------------------------------
+Jacquelyn   |   Jackson     |   DBA                 |
+-----------------------------------------------------------------------
+Sally       |   Weber       |   Web Developer       |   2015-12-18
+
+앞의 직원 명단을 Last Name으로 정렬시킨다음, 그 결과를 표 형태로 출력하는
+프로그램을 작성하라.
+
+출력 예
+Name                | Position          | Separation Date
+--------------------|-------------------|-----------------
+Jacquelyn Jackon    | DBA               |
+Jake Jacobson       | Programmer        |
+John Johnson        | Manager           | 2016-12-31
+Michaela Michaelson | District Manager  | 2015-12-19
+Sally Weber         | Web Developer     | 2015-12-18
+Tou Xiong           | Software Engineer | 2016-10-05
+
+제약 조건
+* 맵 리스트를 사용하여 데이터를 구현할 것
+
+도전 과제
+* 어떻게 정렬시킬 것인지를 입력받도록 하라. 정렬 가능한 기준은 Separation Data,
+  Position, Last Name이 있다.
+* MySQL 같은 데이터베이스나 Redis 같은 키-값 저장소를 사용하여 지원 레코드를
+  저장하라. 물론 데이터 저장소로부터 레코드를 조회해야 한다.
+
+'''
+
+
+
+
+
+if __name__ == "__main__":
+    pass
+
