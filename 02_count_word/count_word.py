b1f529d9b9e350b8c857890178c1c7f2af49b4af
shlomolim90 <shlomolim90@gmail.com>
2019-06-08 13:53:27 +0900

    02_count_word: code refactoring

--- 02_count_word/count_word.py
+++ 02_count_word/count_word.py
@@ -1,32 +1,29 @@
 # problem 2. counting words
+#-*- encoding:utf-8 -*-
 '''
-Description
-The method count() returns the number of occurrences of substring sub in
-the range [start, end]. Optional arguments start and end are interpreted
-as in slice notation.
+글자 수 세기
 
-Syntax
-str.count(sub, start= 0,end=len(string))
-Parameters
-sub -- This is the substring to be searched.
+문자열을 입력 받은 다음 입력 받은 문자열과 문자열의 
+글자수를 출력하는 프로그램을 작성하라.
 
-start -- Search starts from this index. First character starts from 0 index.
-By default search starts from 0 index.
+출력 예
+What is the input string? Homer
+Homer has 5 characters.
 
-end -- Search ends from this index. First character starts from 0 index.
-By default search ends at the last index.
+제약조건
+* (V) 출력 결과에는 입력 받은 문자열이 그대로 나타나도록 할 것.
+* (V) 출력을 위해 하나의 출력문을 사용할 것.
+* (V) 문자열의 길이를 구하기 위해 프로그래밍 언어에서 제공하는 내장 함수를 사용할 것
 
-Return Value
-Centered in a string of length width.
+도전과제
+* (V) 사용자가 아무 것도 입력하지 않은 채로 엔터 키를 치면 무엇이라도 입력하라는 메세지를 나타내보자.
 '''
 
 name = raw_input('what is the input string? ')
-
 name_len = len(name)
-
 if(name_len == 0):
-    print('input something plz')
+    msg = 'input something plz'
 else:
-    print(name+' has '+str(name_len)+' characters.')
-
+    msg = name + ' has ' + str(name_len) + ' characters.'
+print(msg)
 
